69086f22da1248c317cf24911ee8d4ba09869aa4
Vastalie Efekele <vastalieefekele15@gmail.com>
2024-12-10 15:48:49 +0000

    updated index.js

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ app.get('/about', (req, res) => {
   });
 
   // Route for viewing all tasks
-  app.get('/tasks', async (req, res) => {
+  app.get('./tasks', async (req, res) => {
     try {
       // Fetch all tasks from the database
       const [tasks] = await db.query(
@@ -208,7 +208,7 @@ app.get('./tasks/new', (req, res) => {
   res.render('new-task'); // Render the new task creation page
 });
 
-app.get('./tasks', async (req, res) => {
+app.get('/tasks', async (req, res) => {
   try {
       // Fetch all tasks from the database
       const [tasks] = await db.query(

f56700c5169e172f2348ef9eb0bf7a17fab3b3fc
Vastalie Efekele <vastalieefekele15@gmail.com>
2024-12-12 13:02:07 +0000

    updated code

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 
 // Set up Spotify API client
 const spotifyApi = new SpotifyWebApi({
-    clientId: '982fe0781e144e65a57ce7506d00b0e1',
+    clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
 });
 
